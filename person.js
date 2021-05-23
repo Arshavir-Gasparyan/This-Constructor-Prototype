1f435b5d478f8c2d8a4a559f7d97903577f0f871
Arshavir Gasparyan <arshavir2908@gmail.com>
2021-05-23 17:08:40 +0400

    feat: compare person's age

---
+++ person.js
@@ -0,0 +1,21 @@
+class Person {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+  compareAge() {
+    if (this.age > 18) {
+      return `${this.name} is older than me`;
+    } else if (this.age < 18) {
+      return `${this.name} is younger than me`;
+    } else {
+      return `${this.name} is the same age as me`;
+    }
+  }
+}
+const p1 = new Person("Samuel", 24);
+const p2 = new Person("Joel", 17);
+const p3 = new Person("Lily", 18);
+console.log(p1.compareAge());
+console.log(p2.compareAge());
+console.log(p3.compareAge());
