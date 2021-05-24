fadc1b73c3c3f7721e204b7f064fbf2f82f4e520
Arshavir Gasparyan <arshavir2908@gmail.com>
2021-05-24 13:45:51 +0400

    fix: without class

--- calculator.js
+++ calculator.js
@@ -1,16 +1,21 @@
-class Calculator {
-  add(a, b) {
+function Calculator(...[a, b]) {
+  this.a = a;
+  this.b = b;
+  this.add = function (a, b) {
     return a + b;
-  }
-  subtract(a, b) {
+  };
+  this.subtract = function (a, b) {
     return a - b;
-  }
-  multiply(a, b) {
+  };
+  this.multiply = function (a, b) {
     return a * b;
-  }
-  divide(a, b) {
+  };
+  this.divide = function (a, b) {
     return a / b;
-  }
+  };
 }
 const calculator = new Calculator();
+console.log(calculator.add(3, 5));
+console.log(calculator.subtract(3, 5));
 console.log(calculator.multiply(3, 5));
+console.log(calculator.divide(3, 5));
