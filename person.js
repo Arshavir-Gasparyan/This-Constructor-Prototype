ffaecf0ebfef8c4c620ca3adf622b897f5689169
Arshavir Gasparyan <arshavir2908@gmail.com>
2021-05-24 13:25:45 +0400

    fix: without Class

--- person.js
+++ person.js
@@ -1,18 +1,17 @@
-class Person {
-  constructor(name, age) {
-    this.name = name;
-    this.age = age;
-  }
-  compareAge() {
-    if (this.age > 18) {
-      return `${this.name} is older than me`;
-    } else if (this.age < 18) {
-      return `${this.name} is younger than me`;
-    } else {
-      return `${this.name} is the same age as me`;
-    }
-  }
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
 }
+Person.prototype.compareAge = function () {
+  if (this.age > 18) {
+    return `${this.name} is older than me`;
+  } else if (this.age < 18) {
+    return `${this.name} is younger than me`;
+  } else {
+    return `${this.name} is the same age as me`;
+  }
+};
+
 const p1 = new Person("Samuel", 24);
 const p2 = new Person("Joel", 17);
 const p3 = new Person("Lily", 18);
