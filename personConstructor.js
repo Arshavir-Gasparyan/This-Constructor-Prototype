c30a4da8bd51e80a39dde0cee0ab2bf271c93e6e
Arshavir Gasparyan <arshavir2908@gmail.com>
2021-05-24 13:53:46 +0400

    fix: whitout class

--- personConstructor.js
+++ personConstructor.js
@@ -1,29 +1,28 @@
-class Person {
-  constructor(name, age) {
-    this.name = name;
-    this.age = age;
-    this.stomach = [];
-  }
-
-  eat(...a) {
-    if (this.stomach.length < 10) {
-      this.stomach.push(a);
-      return this.stomach;
-    } else {
-      return "i am not hungry";
-    }
-  }
-  poop() {
-    this.stomach.length = 0;
-    return this.stomach;
-  }
-  toString() {
-    return `${this.name},${this.age}`;
-  }
+function Person(name, age, stomach = []) {
+  this.name = name;
+  this.age = age;
+  this.stomach = stomach;
 }
+
+Person.prototype.eat = function (...a) {
+  if (this.stomach.length < 10) {
+    this.stomach = [...this.stomach, ...a];
+    return this.stomach;
+  } else {
+    return "i am not hungry";
+  }
+};
+Person.prototype.poop = function () {
+  this.stomach.length = 0;
+  return this.stomach;
+};
+Person.prototype.toString = function () {
+  return `${this.name},${this.age}`;
+};
+
 const user1 = new Person("Ann", 40);
 const user2 = new Person("Bob", 29);
-console.log(user1.eat("fruits"));
+console.log(user1.eat("fruits", "apple"));
 console.log(user1.eat("something"));
 console.log(user1.poop());
 console.log(user1.toString());
